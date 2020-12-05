5b8ecb9d8e1c81275190e287d261a733239d088e
Sebastian <sebastian.wask@codeagency.pl>
2020-12-05 21:06:28 +0100

    Connecting calc function

---
+++ calculations/multiplication.hpp
@@ -0,0 +1,14 @@
+//
+//  multiplication.hpp
+//  projektutp
+//
+//
+
+#ifndef multiplication_hpp
+#define multiplication_hpp
+
+#include <stdio.h>
+
+int funcMultiplication(int a, int b);
+
+#endif /* additional_hpp */
---
+++ calculations/subtraction.hpp
@@ -0,0 +1,14 @@
+//
+//  substraction.hpp
+//  projektutp
+//
+//
+
+#ifndef subtraction_hpp
+#define subtraction_hpp
+
+#include <stdio.h>
+
+int funcSubtraction(int a, int b);
+
+#endif /* additional_hpp */
