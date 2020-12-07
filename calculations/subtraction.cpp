fe4949e34651cae5ebd1165142fd30a3a4fe5789
IkarOfficial <IkarOfficial@gmail.com>
2020-12-07 15:46:39 +0100

    subtraction float

--- calculations/subtraction.cpp
+++ calculations/subtraction.cpp
@@ -2,7 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int subtraction()
-{
+#include <iostream>
+using namespace std;
+int subtraction(){
+    float num1, num2, result;
+    cout<<"Type number: ";
+    cin>>num1;
+    cout<<"Type second number: ";
+    cin>>num2;
+    result=num1-num2;
+    cout<<"Result: ";
+    return result;
 }
