c0ae31bbe71c9f240cedec73a3df7d9355964695
Sebastian <sebastian.wask@codeagency.pl>
2020-12-20 17:20:54 +0100

    Wprowadzenie poleceń odpowiedzialnych za obsługe kalkulatora

--- main.cpp
+++ main.cpp
@@ -9,11 +9,23 @@
 using namespace std;
 
 #include "./calculations/additional.hpp"
+#include "./calculations/division.hpp"
+#include "./calculations/multiplication.hpp"
+#include "./calculations/subtraction.hpp"
 
 int main()
 {
-    int additional = funcAdditional(5, 10);
-    cout << "final: " << additional;
+    char operate;
+    int num1, num2, result;
+
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> num1;
+
+    cout << "Podaj znak: " << endl;
+    cin >> operate;
+
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> num2;
 
     return 0;
 }
