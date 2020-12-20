6e491dea58eda59442235d8cf1f0f44954e5dce2
IkarOfficial <IkarOfficial@gmail.com>
2020-12-20 17:44:11 +0100

    pierwszy program

--- calculations/subtraction.cpp
+++ calculations/subtraction.cpp
@@ -1,14 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <iostream>
-using namespace std;
-int subtraction(){
-    float num1, num2, result;
-    cout<<"Type number: ";
-    cin>>num1;
-    cout<<"Type second number: ";
-    cin>>num2;
-    result=num1-num2;
-    return result;
+//
+// subtraction.hpp
+// projektutp
+//
+
+#include "subtraction.hpp"
+
+int funcSubtraction(int a, int b)
+{
+    int sum=a-b;
+    return sum;
 }
